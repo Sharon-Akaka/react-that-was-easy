485df0f26822963037ea7c009acd16a6e6eefb47
Sharon Akaka <sharon.akaka@gmail.com>
2021-02-24 11:45:51 +0000

    Created buttons using .map

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,21 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
+  const buttonOptions = ["easy", "ok", "difficult", "too difficult"]
+
+  const newButtons = buttonOptions.map((item) => {
+    return <button onClick={()=>alert(`That was ${item}`)}>{item}</button>
+  }
+  );
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {newButtons}
     </div>
   );
 }
 
 export default App;
+
